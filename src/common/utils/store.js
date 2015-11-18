974ac920e81d3731e8b57d6abfc7a32260a3e3a7
Jogis <zenjayjay@gmail.com>
2015-11-18 09:45:41 +0800

    remove develope tool

--- src/common/utils/store.js
+++ src/common/utils/store.js
@@ -1,14 +1,7 @@
 import {createStore,applyMiddleware,compose} from 'redux';
 //import all your middleware here
 import thunk from 'redux-thunk';
-import { devTools, persistState } from 'redux-devtools';
 export let defaultMiddlewares = [applyMiddleware(thunk)];
-if(config.debug){
-  defaultMiddlewares = defaultMiddlewares.concat([
-    devTools(),
-    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-  ])
-}
 
 export function createStoreWithMiddleware(middlewares = []){
   return function(...args){
