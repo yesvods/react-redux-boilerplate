ed8704e766105931f4b22e0754db456becfee86a
Eyas <liuyuesongde@163.com>
2015-12-08 09:30:36 +0800

    Signed-off-by: Eyas <liuyuesongde@163.com>

--- webpack.config.js
+++ webpack.config.js
@@ -34,9 +34,7 @@ module.exports = {
         loader: 'react-hot!babel'
       },
       {test: /\.json$/, loader: 'json'},
-      { test: /\.s[c|a]ss$/, 
-        loader: 'style!css?modules&importLoaders=2&localIdentName=[local]___[hash:base64:5]!sass'
-      },
+      { test: /\.s[c|a]ss$/, loader: 'style!css?modules&importLoaders=2&localIdentName=[local]___[hash:base64:5]!sass'},
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.(png|jpg|jpeg)$/, loader: 'url-loader?limit=8192' },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
